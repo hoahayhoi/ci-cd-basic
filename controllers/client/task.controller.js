e4a2f5d2a786f9bb5dd1b3e20f300a1f229914ad
hoahayhoi <hoa.tmh2003@gmail.com>
2025-01-07 11:11:11 +0700

    .

---
+++ controllers/client/task.controller.js
@@ -0,0 +1,144 @@
+const Task = require("../../models/task.model");
+
+module.exports.index = async (req, res) => {
+  const find = {
+    $or: [
+      { createdBy: req.user.id },
+      { listUser: req.user.id }
+    ],
+    deleted: false
+  }
+
+  if (req.query.status) {
+    find.status = req.query.status;
+  }
+
+  // Sort 
+  const sort = {};
+
+  if (req.query.sortKey && req.query.sortValue) {
+    sort[req.query.sortKey] = req.query.sortValue;
+  }
+  // End sort 
+
+  // Pagination 
+  let limitItem = 4;
+  let page = 1;
+
+  if (req.query.page) {
+    page = req.query.page;
+  }
+
+  if (req.query.limitItem) {
+    limitItem = req.query.limitItem;
+  }
+
+  const skip = (page - 1) * limitItem;
+  // End Pagination 
+
+  // Search
+  if (req.query.keyword) {
+    const regex = new RegExp(req.query.keyword, "i");
+    find.title = regex;
+  }
+  // End Search
+
+  const tasks = await Task
+    .find(find)
+    .limit(limitItem)
+    .skip(skip)
+    .sort(sort);
+
+  res.json({
+    code: "success",
+    message: "Thành công!",
+    data: tasks
+  });
+}
+
+module.exports.detail = async (req, res) => {
+  const id = req.params.id;
+
+  const task = await Task.findOne({
+    _id: id,
+    $or : [
+      { createdBy: req.user.id }, 
+      { listUser: req.user.id }
+    ],
+    deleted: false
+  });
+
+  if(!task) {
+    res.json({
+      code: "error",
+      message: "Id không hợp lệ!"
+    });
+    return;
+  }
+
+  res.json({
+    code: "success",
+    message: "Thành công!",
+    data: task
+  });
+}
+
+module.exports.changeMultiPatch = async (req, res) => {
+  const status = req.body.status;
+  const ids = req.body.ids;
+
+  await Task.updateMany({
+    _id: { $in: ids }
+  }, {
+    status: status
+  });
+
+  res.json({
+    code: "success",
+    message: "Thành công!"
+  });
+}
+
+module.exports.createPost = async (req, res) => {
+  const data = req.body;
+
+  data.createdBy = req.user.id;
+
+  const task = new Task(data);
+  await task.save();
+
+  res.json({
+    code: "success",
+    message: "Tạo công việc thành công!",
+    data: task
+  });
+}
+
+module.exports.editPatch = async (req, res) => {
+  const id = req.params.id;
+  const data = req.body;
+
+  await Task.updateOne({
+    _id: id
+  }, data);
+
+  res.json({
+    code: "success",
+    message: "Cập nhật công việc thành công!"
+  });
+}
+
+module.exports.deleteMultiPatch = async (req, res) => {
+  const ids = req.body.ids;
+
+  await Task.updateMany({
+    _id: { $in: ids }
+  }, {
+    deleted: true
+  });
+
+  res.json({
+    code: "success",
+    message: "Xoá thành công!"
+  });
+}
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+require('dotenv').config();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const app = express();
+const port = 3000;
+
+const databse = require("./config/database");
+databse.connect();
+
+const routeClient = require("./routes/client/index.route");
+
+// parse application/json
+app.use(bodyParser.json());
+
+routeClient(app);
+
+// // Tất cả tên miền được phép truy cập vào
+app.use(cors());
+
+// Cho phép 1 tên miền cụ thể được phép truy cập
+// const corsOptions = {
+//   origin: 'http://example.com',
+//   optionsSuccessStatus: 200
+// }
+// cors(corsOptions);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
